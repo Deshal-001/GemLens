34235ba166fbc9924bea3bfe71cdc7483584005c
Kavindu Deshal <85722430+Deshal-001@users.noreply.github.com>
2024-03-29 16:49:46 +0530

    Backend
    Backend

--- gemlens_backend/app.py
+++ gemlens_backend/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, request, jsonify, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+import numpy as np
+import onnxruntime as ort
+from flask import Flask, request
+from keras.preprocessing import image
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY']= 'mysecretkey'
+sess = ort.InferenceSession('/Users/kavindudeshalsilva/Documents/GitHub/GemLens/gemlens_backend/onnx_model.onnx')
 
+@app.route('/predict', methods=['GET'])
+def predict():
+    img_path = '/Users/kavindudeshalsilva/Documents/GitHub/GemLens/gemlens_backend/Amethyst_Test_106.jpg'
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = x/255.0
+    input_name = sess.get_inputs()[0].name
+    classes = sess.run(None, {input_name: x})
+    return str(np.argmax(classes[0]))
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return "Hello World!"
+if __name__ == '__main__':
+    app.run(debug=True)
